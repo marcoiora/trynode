4aac845c604b2e07a6495d7ae292078cd4a43b69
marcoiora <marco.iora@gmail.com>
2011-09-15 17:14:26 +0000

    finally first parser working

--- template.js
+++ template.js
@@ -1,56 +1,57 @@
 var fs = require('fs');
 
 function Template(path) {
-    var self = this,
-        output, buffer;
-        
-    var loaded = false;
-    var error;
-    
-	fs.readFile(path, function(err, data) {
-		if (!err) {
-            data = data.utf8Slice().replace(/(.*)(?!<%.*%>)/g, function(matched, p, index) {
-				console.log(matched);
-				
-                lines = p.split("\n");
-                outtext = "";
-                for (i = 0; i < lines.length; i++) {
-                    outtext += "buffer += \""+lines[i].replace(/"/g,"\\\"")+"\\n\";\n";
-                }
-                return outtext;
-            });
-			
-            data = data.replace(/<% (.*) %>/g, function(matched,p,index) {
-                return p;
-            });
-            data = data.replace(/<%= (.*) %>/g, function(matched,p,index) {
-                return "buffer += ("+p+");";
-            });
-            
-            
-            /*
-            lines = data.utf8Slice().split("\n");
-            var output = "";
-            for (var i = 0; i < lines.length; i++) {
-                var line = lines[i];
-                if (line.match(/^- .+/)) {
-                    output += line.replace(/^- /,"");
-                } else {
-                    output += "buffer += \""+line.replace(/"/g,"\\\"")+"\";";
-                }
-            }*/
-		} else {
-            loaded = false;
-            error = err;
-            console.log('err');
-		}
-	});
+    this.path = path;
+    this.tmpl = "";
+    this.loaded = false;
+    this.error;
+    this.step = 0;
 }
 
 Template.prototype = {
-    render : function(context) {
-        eval.apply(context,output);
-    }
+    render : function() {
+        var self = this;
+        this.step = 1;
+        if (!this.loaded) {
+            fs.readFile(this.path, this.load(this));
+        }
+    },
+    toString : function() {
+        return this.constructor.name + " loaded=" + this.loaded + " tmpl=" + this.tmpl+ " step="+this.step;
+    },
+    load : function(context) {
+        var self = context;
+        return function(err, data) {
+        
+        if (!err) {
+            data = data.toString();
+            var prev = 0;
+            var cur = 0;
+            self.tmpl = "";
+            while ((cur = data.indexOf("<%",prev)) > -1) {
+                if (cur > prev) {
+                    var plain = data.substring(prev,cur);
+                    self.tmpl = self.tmpl + "buffer += \""+plain.replace(/"/g,"\\\"").replace(/\n/,"\\n")+"\";";
+                }
+                prev = data.indexOf("%>",cur);
+                var code = data.substring(cur+3,prev);
+                if (data[cur+2] == "=") {
+                    self.tmpl = self.tmpl + "buffer += " + code.trim() + ";";
+                } else {
+                    self.tmpl = self.tmpl + code.trim();
+                }
+                prev = prev+2;
+            }
+            if (prev >= 0) {
+                var plain = data.substring(prev);
+                self.tmpl = self.tmpl + "buffer += \""+plain.replace(/"/g,"\\\"").replace(/\n/,"\\n")+"\";";
+            }
+            self.loaded = true;
+    	} else {
+            self.loaded = false;
+            self.error = err;
+		}
+    }}
 }
 
 exports.Template = Template;
--- testtemplate.js
+++ testtemplate.js
@@ -1,17 +1,17 @@
 var template = require('./template');
 var http = require('http');
 
-t = new template.Template('tmpl/tmpl.txt');
-
+var t = new template.Template('tmpl/tmpl.txt');
+t.render();
+setTimeout(function() {console.log("helloworld");}, 1000);
+setTimeout(function() {console.log(t.tmpl.replace(/</g,"&lt;").replace(/>/,"&gt;"));}, 2000);
+/*
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
     t = new template.Template('tmpl/tmpl.txt');
-    /*template.Template('tmpl/tmpl.txt', function(tmpl) {
-        res.writeHead(200);
-        titolo = "TITOLO";
-        corpo = "CORPO";
-        eval(tmpl);
-        res.end("");
-    });*/
+    t.render();
+    ;
+    res.end(t.tmpl);
 });
 server.listen(process.env.C9_PORT,'0.0.0.0');
+*/
