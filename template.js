bd17d13816ed62ca83c923ca80fc686e4f0b5381
marcoiora <marco.iora@gmail.com>
2011-09-17 15:18:17 +0000

    working template rendering

--- template.js
+++ template.js
@@ -1,58 +1,90 @@
 var fs = require('fs');
 
-function Template(path) {
+function Template(path, preload) {
     this.path = path;
     this.tmpl = "";
     this.loaded = false;
     this.error;
-    this.step = 0;
+    this.loading = false;
+    this.loadingcallbacks = [];
+    
+    if (preload) {
+        this.load(function(){});
+    }
 }
 
 Template.prototype = {
-    render : function() {
+    load : function(callback) {
+        console.log("called load");
+        if (this.loading)
+            return;
+        this.loading = true;
+        
         var self = this;
-        this.step = 1;
+        
+        fs.readFile(this.path, function(err, data) {
+            if (!err) {
+                data = data.toString();
+                var prev = 0;
+                var cur = 0;
+                self.tmpl = "";
+                while ((cur = data.indexOf("<%",prev)) > -1) {
+                    if (cur > prev) {
+                        var plain = data.substring(prev,cur);
+                        self.tmpl = self.tmpl + "this.buffer += \""+plain.replace(/"/g,"\\\"").replace(/\n/g," ")+"\";";
+                    }
+                    prev = data.indexOf("%>",cur);
+                    var code = data.substring(cur+3,prev);
+                    if (data[cur+2] == "=") {
+                        self.tmpl = self.tmpl + "this.buffer += " + code.trim() + ";";
+                    } else {
+                        self.tmpl = self.tmpl + code.trim();
+                    }
+                    prev = prev+2;
+                }
+                if (prev >= 0) {
+                    var plain = data.substring(prev);
+                    self.tmpl = self.tmpl + "this.buffer += \""+plain.replace(/"/g,"\\\"").replace(/\n/g," ")+"\";";
+                }
+                self.loaded = true;
+                callback.call(self);
+            } else {
+                self.loaded = false;
+                self.error = err;
+                callback.call(self);
+		    }
+        });
+    },
+    render : function(context, callback) {
+        console.log("called render");
+        var self = this;
+        var context = context;
+        var callback = callback;
+        // ensure tmpl is loaded
         if (!this.loaded) {
-            fs.readFile(this.path, this.load(this));
+            console.log("calling load from render");
+            this.load(function() {
+                console.log("finished loadin");
+                self.render(context,callback)});
+            return;
+        } else {
+            var container = {};
+            container.buffer = "";
+            for (var attrname in context) {
+                container[attrname] = context[attrname];
+            }
+            container.dotmpl = function(tmpl) {
+                console.log(tmpl);
+                eval(tmpl);
+            }
+            container.dotmpl(this.tmpl);
+            //eval.call(container,this.tmpl);
+            callback.call(this,container.buffer);
         }
     },
     toString : function() {
         return this.constructor.name + " loaded=" + this.loaded + " tmpl=" + this.tmpl+ " step="+this.step;
-    },
-    load : function(context) {
-        var self = context;
-        return function(err, data) {
-        
-        if (!err) {
-            data = data.toString();
-            var prev = 0;
-            var cur = 0;
-            self.tmpl = "";
-            while ((cur = data.indexOf("<%",prev)) > -1) {
-                if (cur > prev) {
-                    var plain = data.substring(prev,cur);
-                    self.tmpl = self.tmpl + "buffer += \""+plain.replace(/"/g,"\\\"").replace(/\n/,"\\n")+"\";";
-                }
-                prev = data.indexOf("%>",cur);
-                var code = data.substring(cur+3,prev);
-                if (data[cur+2] == "=") {
-                    self.tmpl = self.tmpl + "buffer += " + code.trim() + ";";
-                } else {
-                    self.tmpl = self.tmpl + code.trim();
-                }
-                prev = prev+2;
-            }
-            if (prev >= 0) {
-                var plain = data.substring(prev);
-                self.tmpl = self.tmpl + "buffer += \""+plain.replace(/"/g,"\\\"").replace(/\n/,"\\n")+"\";";
-            }
-            self.loaded = true;
-    	} else {
-            self.loaded = false;
-            self.error = err;
-		}
-    }}
+    }
 }
 
 exports.Template = Template;
-
--- testtemplate.js
+++ testtemplate.js
@@ -1,17 +1,19 @@
 var template = require('./template');
 var http = require('http');
 
-var t = new template.Template('tmpl/tmpl.txt');
-t.render();
-setTimeout(function() {console.log("helloworld");}, 1000);
-setTimeout(function() {console.log(t.tmpl.replace(/</g,"&lt;").replace(/>/,"&gt;"));}, 2000);
-/*
+console.log("creating instance");
+var t = new template.Template('tmpl/tmpl.txt', false);
+console.log("calling render");
+
+//setTimeout(function() {console.log("helloworld");}, 1000);
+//setTimeout(function() {console.log(t.tmpl.replace(/</g,"&lt;").replace(/>/,"&gt;"));}, 2000);
+
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    t = new template.Template('tmpl/tmpl.txt');
-    t.render();
-    ;
-    res.end(t.tmpl);
+    t.render({titolo: 'titolone', corpo: 'corpone'}, function(buffer) {
+        res.end(buffer);
+    });
+    
 });
 server.listen(process.env.C9_PORT,'0.0.0.0');
-*/
+
